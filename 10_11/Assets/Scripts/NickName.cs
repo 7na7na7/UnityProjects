2f553adb9c186d3067d3830d6da9265f7e759e45
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-10-11 16:58:18 +0900

    explosion1

---
+++ 10_11/Assets/Scripts/NickName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NickName : MonoBehaviour
+{
+    private NetworkManager net;
+
+    private void Start()
+    {
+        net = FindObjectOfType<NetworkManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            net.Connect();
+        }
+    }
+}
