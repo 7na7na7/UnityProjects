88598c047b0ad63ac501f81028d0d28a46b36923
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-10-23 17:19:18 +0900

    10_23_자동차게임제작시작!

---
+++ CarGame_M/Assets/Scripts/CarMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 10;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = speed;
+    }
+
+    void Update()
+    {
+        
+    }
+}
