2f821864064cbf0fd537ca0cc20df05ae676a6d0
SexyM <hmg6435573@gmail.com>
2019-12-26 15:20:45 +0900

    올라프 옷입히기!

---
+++ OlafDressup/Assets/Scenes/Drag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    //오브젝트에 콜라이더(클릭범위) 넣어주고
+       void OnMouseDrag() //드래그 중일 때
+       {
+           if (Input.GetMouseButtonDown(1))
+           {
+               Destroy(gameObject);
+           }
+           Vector3 mousePos3D = Input.mousePosition;
+           this.transform.position = Camera.main.ScreenToWorldPoint(mousePos3D);
+           transform.position=new Vector3(transform.position.x,transform.position.y,0);
+       }
+}
