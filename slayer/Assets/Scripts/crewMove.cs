b1f280fc7fdb055bd218e3e633c1463c7701a8f3
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-03-30 15:14:32 +0900

    3_30_거미완성중간커밋

--- slayer/Assets/Scripts/crewMove.cs
+++ slayer/Assets/Scripts/crewMove.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class crewMove : MonoBehaviour
 {
+    public bool isAngle = false;
     private Vector2 dir;
     private void Start()
     {
+        if (isAngle)
+        {
+            transform.eulerAngles =
+                new Vector3(0, 0, -getAngle(transform.position.x, transform.position.y, Player.instance.transform.position.x, Player.instance.transform.position.y)+180);
+        }
         dir = Player.instance.transform.position - transform.position;
         dir.Normalize();
         Destroy(gameObject,5f);
@@ -12,6 +19,23 @@ public class crewMove : MonoBehaviour
 
     void Update()
     {
-       transform.Translate(dir*Time.deltaTime*10);
+        transform.position += (Vector3)dir * Time.deltaTime * 10;
+       //transform.Translate(dir*Time.deltaTime*10);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Ground"))
+            Destroy(gameObject);
+    }
+    private float getAngle(float x1, float y1, float x2, float y2) //Vector값을 넘겨받고 회전값을 넘겨줌
+    {
+        float dx = x2 - x1;
+        float dy = y2 - y1;
+
+        float rad = Mathf.Atan2(dx, dy);
+        float degree = rad * Mathf.Rad2Deg;
+        
+        return degree;
     }
 }
