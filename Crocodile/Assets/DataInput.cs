5c7b200cc1df3057860dae97a4642fd88ed2ba33
SexyM <hmg6435573@gmail.com>
2020-06-26 15:44:02 +0900

    6_26_악어게임외주

---
+++ Crocodile/Assets/DataInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInput : MonoBehaviour
+{
+    public GameObject InputPanel;
+    public InputField input;
+
+
+    public void Input()
+    {
+        if (input.text == "1" || input.text == "2" || input.text == "3" || input.text == "4" || input.text == "5" ||
+            input.text == "6" || input.text == "7" || input.text == "8" || input.text == "9")
+        {
+            FindObjectOfType<GameManager>().Set(int.Parse(input.text));
+            
+            InputPanel.SetActive(false);
+        }
+    }
+}
---
+++ Crocodile/Assets/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip clip;
+    public AudioClip clip2;
+    public Sprite pressedTooth;
+    public Sprite pressedCrocodile;
+    public Image crocodile;
+    public GameObject[] toothImages;
+    public bool[] tooths;
+    public GameObject CrocodileText;
+    public GameObject GameOverPanel;
+    public void Set(int correctTooth)
+    {
+        for (int i = 0; i < tooths.Length; i++)
+        {
+            tooths[i] = false;
+        }
+
+        int a = 0;
+        int r = 0;
+        while (a<correctTooth)
+        {
+          
+            while (true) 
+            {
+                r = Random.Range(0, tooths.Length);
+                if (tooths[r] != true)
+                    break;
+            }
+          
+            tooths[r] = true;
+            a++;
+        }
+    }
+
+
+   public void Press(int value)
+   {
+       toothImages[value].GetComponent<Image>().sprite = pressedTooth;
+       source.PlayOneShot(clip2);
+       if (tooths[value] == true)
+       {
+           foreach (GameObject tooth in toothImages)
+           {
+               tooth.SetActive(false);
+           }
+           crocodile.sprite = pressedCrocodile;
+           CrocodileText.SetActive(true);
+           source.PlayOneShot(clip);
+           StartCoroutine(delay());
+       }
+   }
+
+   IEnumerator delay()
+   {
+       yield return new WaitForSeconds(1f);
+       GameOverPanel.SetActive(true);
+   }
+   public void Exit()
+   {
+       Application.Quit();
+   }
+
+   public void Restart()
+   {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+   }
+}
