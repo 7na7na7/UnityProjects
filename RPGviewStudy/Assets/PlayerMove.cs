25acf4d289ac526ae0310924a29c46c53c7060d5
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-07-08 22:49:02 +0900

    2D extra, 픽셀 퍼펙트 카메라를 이용한 타일맵 제작과 픽셀 안깨지는법

---
+++ RPGviewStudy/Assets/PlayerMove.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal")*5*Time.deltaTime,Input.GetAxisRaw("Vertical")*5*Time.deltaTime,0);
+    }
+}
