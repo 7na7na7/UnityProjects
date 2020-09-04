da3771aa6b6191be7c36f907c959f51dbc3b7bba
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-09-05 01:25:59 +0900

    9_5_구글플레이세팅 잘된것같다^^

--- STAC____RE/Assets/GooglePlayGames/GameInfo.cs
+++ STAC____RE/Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "473631222903"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "473631222903-4ar8mr9c4ahdjut44dhh0b5fjf8a1c9i.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
