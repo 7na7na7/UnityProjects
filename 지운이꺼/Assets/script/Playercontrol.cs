34d1cb97696258a48584d18485f02f559e8c1994
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-05-04 23:50:22 +0900

    지운이꺼 1,2 칸료.

--- 지운이꺼/Assets/script/Playercontrol.cs
+++ 지운이꺼/Assets/script/Playercontrol.cs
@@ -26,7 +26,15 @@ public class Playercontrol : MonoBehaviour
     public GameObject sword;
     public bool canMove = true;
     private const float MoveSize = 5f;
-    
+    public Transform wallcheak;
+    public LayerMask w_layer;
+    public bool iswall_jump;
+    public bool iswall;
+    public float is_right;
+    public float slide_speed;
+    public float wallch_distanse;
+    public float walljump_power;
+    public bool is_attack;
     Vector2 move;
     SpriteRenderer sr;
 
@@ -41,12 +49,18 @@ public class Playercontrol : MonoBehaviour
 
     void Update()
     {
-        if(move.x==0)
+        if (move.x == 0)
         {
-            if(rig.velocity.y<=0.01f)
+            if (rig.velocity.y <= 0.01f)
                 rig.velocity = new Vector2(0, rig.velocity.y);
         }
-           
+
+
+        //wall slide
+        iswall = Physics2D.Raycast(wallcheak.position, is_right * Vector2.right, wallch_distanse, w_layer);
+        animator.SetBool("is_slide", iswall);
+
+
         //is_ground = Physics2D.OverlapCircle(chkPos.position, check_Radius, ground_mask);
         move.x = Input.GetAxisRaw("Horizontal");
 
@@ -59,9 +73,10 @@ public class Playercontrol : MonoBehaviour
             animator.SetBool("is_walk", false);
         }
 
-        
-        if (Input.GetButtonDown("Fire1"))
+
+        if (Input.GetButtonDown("Fire1")&& !is_attack)
         {
+            is_attack = true;
             sword.SetActive(true);
             animator.SetTrigger("attack");
             AttackDash();
@@ -71,6 +86,20 @@ public class Playercontrol : MonoBehaviour
             Vector2 input = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 dir = input - (Vector2)transform.position;
             dir.Normalize();
+
+            if (dir.x > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                left_check = false;
+                is_right = 1;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                left_check = true;
+                is_right = -1;
+            }
+            
             if (dir.y <= 0.3f)
                 dir.y = 0.3f;
             rig.velocity = dir * 10f;
@@ -82,6 +111,7 @@ public class Playercontrol : MonoBehaviour
         {
             yield return new WaitForSeconds(0.3f);
             canMove = true;
+            is_attack = false;
         }
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && is_dash == false && dash_count < 2)
@@ -114,25 +144,14 @@ public class Playercontrol : MonoBehaviour
 
 
 
-       
-        if (Input.GetButtonDown("Jump") && is_ground == true)
+
+        if (Input.GetButtonDown("Jump") && is_ground == true && iswall == false)
         {
-            is_ground = false;
-            is_jump = true;
-            rig.AddForce(Vector2.up * jump_power, ForceMode2D.Impulse);
-            animator.SetBool("is_jump", true);
-            
-            if (rig.velocity.y > 0)
-            {
-
-                animator.SetBool("is_onair", true);
-
-            }
-
+            Jump();
         }
 
 
-       
+
         if (rig.velocity.y < 0)
         {
             //animator.SetBool("is_onair", false);  
@@ -140,7 +159,9 @@ public class Playercontrol : MonoBehaviour
 
             if (is_ground == true)
             {
+                
                 animator.SetTrigger("ground");
+                
                 is_jump = false;
                 /*
                 animator.SetBool("is_droping", false);
@@ -161,41 +182,83 @@ public class Playercontrol : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (canMove)
+        if (canMove && !iswall_jump)
         {
-            if(!is_jump)
-                rig.velocity=new Vector2(0,rig.velocity.y);
-            transform.position += new Vector3(move.x * speed * Time.deltaTime, 0);   
+            if (!is_jump)
+                rig.velocity = new Vector2(0, rig.velocity.y);
+            transform.position += new Vector3(move.x * speed * Time.deltaTime, 0);
+
         }
         Flip();
+        //wallslide
 
+        if (iswall&& is_jump==true)
+       {
+           //iswall_jump = false;
+          
+           animator.SetBool("is_jump", false);
+           rig.velocity = new Vector2(rig.velocity.x, rig.velocity.y * slide_speed);
+           //walljump
+
+           if (Input.GetButtonDown("Jump") && !iswall_jump)
+           {
+               iswall_jump = true;
+               Invoke("FreezeX",0.3f);
+               rig.velocity=Vector2.zero;
+               rig.velocity = new Vector2(is_right * walljump_power*-1,  walljump_power);
+               //rig.velocity = new Vector2(is_right * walljump_power, 0.9f * walljump_power);
+               Flip();
+
+           }
+       }
+    }   
+
+
+    //wall jump delay
+    void FreezeX()
+    {
+        iswall_jump = false;
     }
-
     //filp
     void Flip()
     {
-        if (rig.velocity.x > 0|| move.x>0)
+        if (is_attack)
+            return;
+        if (rig.velocity.x > 0 || move.x > 0)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
             left_check = false;
+            is_right = 1;
         }
-        else if (rig.velocity.x < 0 || move.x<0)
+        else if (rig.velocity.x < 0 || move.x < 0)
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
             left_check = true;
+            is_right = -1;
         }
     }
+
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
 
-                animator.SetTrigger("ground");
-                is_jump = false;
-                is_ground = true;
+            animator.SetTrigger("ground");
+            is_jump = false;
+            is_ground = true;
 
         }
-            
+
+    }
+    void Jump()
+    {
+        is_ground = false;
+        is_jump = true;
+        rig.AddForce(Vector2.up * jump_power, ForceMode2D.Impulse);
+        animator.SetBool("is_jump", true);
+
+        
+
     }
 }
