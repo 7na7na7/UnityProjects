e9de4cc4353e753dca667522bbbd3f601f2704d8
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-06-10 18:03:57 +0900

    잡시스템 공부! 어렵지만 쓰면 엄청 빨라질꺼같다!

---
+++ JobSystemStudy/Assets/JobSystemStudy/AsyncTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+public class AsyncTest : MonoBehaviour
+{
+    private async void Start()
+    {
+        print("현재 쓰레드 : "+Thread.CurrentThread.ManagedThreadId);
+        //현재 쓰레드 Id 표시, 1이라면 메인 쓰레드
+        
+        print("현재 프레임 : "+Time.frameCount);
+        await TestAsync();
+        print("현재 프레임 : "+Time.frameCount);
+    }
+
+    //void로 해도 댄다. 그럼 async안붙여도 된다!
+    async Task TestAsync()
+    {
+        //await써야 기다려진다!
+        await Task.Run(() =>
+        {
+            print("작업 했음");
+            print("현재 쓰레드 : "+Thread.CurrentThread.ManagedThreadId);
+            //다른 쓰레드에서 작업함
+        });
+    }
+}
