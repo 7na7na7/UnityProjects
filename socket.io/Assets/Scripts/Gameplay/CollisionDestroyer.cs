33396e62399600097626992fab534b94a683df8c
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-06-15 14:47:23 +0900

    내나름대로 OnTrigger도 되게

--- socket.io/Assets/Scripts/Gameplay/CollisionDestroyer.cs
+++ socket.io/Assets/Scripts/Gameplay/CollisionDestroyer.cs
@@ -22,6 +22,17 @@ namespace Project.Gameplay
                 })));
             }
         }
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            NetworkIdentity ni = col.gameObject.GetComponent<NetworkIdentity>();
+            if (ni == null || ni.GetID() != whoActivedMe.GetActivator()) //충돌체가 서버오브젝트가 아니거나, 서버오브젝트인데 나 자신이 아니라면
+            {
+                networkIdentity.GetSocket().Emit("collisionDestroy",new JSONObject(JsonUtility.ToJson(new IDdata
+                {
+                    id=networkIdentity.GetID()
+                })));
+            }
+        }
     }
 }
 
