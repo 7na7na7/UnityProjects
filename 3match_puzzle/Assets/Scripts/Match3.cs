549c9216c96dd0666194f66d9cb6af44ff5be506
nana <hmg6435573@gmail.com>
2021-08-09 02:38:58 +0900

    20분

--- 3match_puzzle/Assets/Scripts/Match3.cs
+++ 3match_puzzle/Assets/Scripts/Match3.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Match3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int width = 9;
+    public int height = 14;
+
+    Node[,] board;
+
+    System.Random random;
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void StartGame()
     {
-        
+        board = new Node[width, height];
+
+        string seed = getRandomSeed();
+        random = new System.Random(seed.GetHashCode());
+
+        initializeBoard();
+    }
+
+    void initializeBoard()
+    {
+        board = new Node[width, height];
+        for(int y=0;y<height;y++)
+        {
+            for(int x=0;x<width;x++)
+            {
+                board[x, y] = new Node(-1,new Point(x,y));
+            }
+        }
+    }
+    string getRandomSeed()
+    {
+        string seed = "";
+        string acceptableChars = "ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+        for(int i=0;i<20;i++)
+            seed += acceptableChars[Random.Range(0, acceptableChars.Length)];
+       
+        return seed;
+    }
+}
+
+[System.Serializable]
+public class Node
+{
+    public int value; //0=빈칸, 1=네모, 2=동그라미, 3=동그라미통, 4=세모, 5=다이아몬, -1=구멍
+    public Point index;
+    public Node(int v,Point i)
+    {
+        value = v;
+        index = i;
     }
 }
