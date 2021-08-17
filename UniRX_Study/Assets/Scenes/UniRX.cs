02338c451aaadb17acf33faefc2ba813841d686f
nana <hmg6435573@gmail.com>
2021-08-17 11:16:27 +0900

    AsObservable,코루틴,UGUI

--- UniRX_Study/Assets/Scenes/UniRX.cs
+++ UniRX_Study/Assets/Scenes/UniRX.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UniRx;
+using UniRx; //이거있어야 UniRx사용가능
+using UniRx.Triggers; //이거있어야 Trigger사용가능
+using UnityEngine.UI;
 
 public class UniRX : MonoBehaviour
 {
     void Start()
     {
         //SubjectSample();
-        ReactivePropSample();
+        //ReactivePropSample();
+        //ObservableSample();
+        CoroutineRxSample();
+        UGUIRxSample();
     }
 
 
@@ -42,5 +47,52 @@ public class UniRX : MonoBehaviour
         //Subscribe 사용가능!
         rp.Subscribe(x => Debug.Log(x + "로 값이 변경되었어요!")); //구독 설정
         rp.Value = 10;
+    }
+
+    //Update대용으로 사용할 함수
+    void ObservableSample()
+    {
+        //this.LateUpdateAsObservable
+        //this.FixedUpdateAsObservable
+        //this.OnTriggerEnter2DAsObservable
+        //위처럼 Observable은 다양한 타이밍이 존재한다.
+
+        //this.UpdateAsObservable().Subscribe(_ =>
+        //Debug.Log("Update실행!")
+        //);
+
+        this.OnTriggerEnter2DAsObservable().Subscribe(col =>
+        print("충돌한 오브젝트의 이름 : "+col.name)
+        );
+    }
+
+    //코루틴이랑 같이쓰기
+    void CoroutineRxSample()
+    {
+        //Coroutine 함수를 Observable 객체로 변환
+        Observable.FromCoroutine<int>(observer => GameTimerCoroutine(observer, 10))
+            .Subscribe(t =>Debug.Log(t==0 ? "끗!" :"카운트다운 "+t+"!"));
+    }
+    IEnumerator GameTimerCoroutine(System.IObserver<int> observer,int initialCount)
+    {
+        var count = initialCount; //100으로 카운트 초기화
+        while(count>0)
+        {
+            observer.OnNext(count--); //1초마다 1씩 감소시켜서 보냄
+            yield return new WaitForSeconds(1);
+        }
+        observer.OnNext(0); //끄
+    }
+
+
+    //UGUI랑 같이쓰기
+    void UGUIRxSample()
+    {
+        var button = GameObject.Find("버튼").GetComponent<Button>();
+        button.OnClickAsObservable().Subscribe(x=>
+        print("버튼이 눌렸어요!!!")); //Button의 OnClick에 해당
+
+        //var slider = GetComponent<Slider>();
+        //slider.OnValueChangedAsObservable().Subscribe(); //Slider의 OnValueChanged()에 해당
     }
 }
